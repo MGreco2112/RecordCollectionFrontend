95686eb7c2afb12898cff24b1a59f48cfd4e68da
Matthew Greco <mgreco2112@tutanota.com>
2022-03-18 23:51:18 -0400

    reajusted home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,8 @@ import Container from '../common/Container';
 const Home = (props) => {
     return (
         <Container>
+            <h1/>
+            <h1/>
             <h1>Welcome to The Vinyl Hub</h1>
             <h2>Connecting Vinyl Enthusiasts since 2022</h2>
         </Container>
