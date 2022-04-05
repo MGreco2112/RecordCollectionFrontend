0940ca4888c7825abe8e02103c53c09a9d69307b
Matthew Greco <mgreco2112@tutanota.com>
2022-04-04 21:00:01 -0400

    began work on displaying records from backend

---
+++ src/components/Records/DisplayRecord.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect, useContenxt, Fragment} from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import {AuthContext} from "../Providers/AuthProvider"
+import Button from "../common/Button"
+import Spinner from "../faCommon/Spinner"
+import ApiHostURL from "../../config"
+
+const DisplayRecord = (props) => {
+
+    const params = useParams();
+    const [record, setRecord] = useState({
+        id: params.recordId,
+    });
+
+    const [loading, setLoading] = useState(true);
+
+    const [auth] = useContenxt(AuthContext);
+
+    useEffect(() => {
+        const _fetchRecord = async () => {
+                const res = await axios.get(`${ApiHostURL}/api/records/${record.id}`, {
+                    headers : {
+                        Authorization: `Bearer ${auth.token}`
+                    }
+                }
+            )
+            console.log("Record" + "\n" + res.data);
+            setRecord(res.data);
+            setLoading(false);
+
+        }
+
+        setLoading(true);
+        _fetchRecord();
+
+    }, [])
+
+
+
+    return (
+        <h1>Display Records</h1>
+    )
+}
+
+export default DisplayRecord;
