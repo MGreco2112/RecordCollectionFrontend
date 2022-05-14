6ece2811660bdf412681c1368f7730d1cda8d46e
Matthew Greco <mgreco2112@tutanota.com>
2022-05-14 14:38:24 -0400

    edit Collector begun

---
+++ src/components/Collector/EditCollector.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect, useContext} from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { apiHostURL } from "../../config";
+import { AuthContext } from "../Providers/AuthProvider";
+import axios from "axios";
+import Form from "../common/Form";
+import InlineInputContainer from "../common/InlineInputContainer";
+import Button from "../common/Button";
+import Container from "../common/Container";
+
+const EditCollector = (props) => {
+    return(
+        <h1>EditCollector</h1>
+    )
+}
+
+export default EditCollector;
