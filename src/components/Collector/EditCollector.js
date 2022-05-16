5265d075320bee904ab47b32a13e1e2cf17b5730
Matthew Greco <mgreco2112@tutanota.com>
2022-05-16 12:40:45 -0400

    planning for EditCollector

--- src/components/Collector/EditCollector.js
+++ src/components/Collector/EditCollector.js
@@ -17,6 +17,60 @@ const EditCollector = (props) => {
         name: ""
     });
 
+    const [editUser, setEditUser] = useState({
+        username: "",
+        password: ""
+    });
+
+    let records = [];
+
+    let comments = [];
+
+    let loading = true;
+
+    useEffect( async () => {
+        if (auth.token) {
+
+            try {
+                const res = await axios.get(`${apiHostURL}/api/collectors/currentCollector`, {
+                    headers: {
+                        Authorization: `Bearer ${auth.token}`
+                    }
+                });
+
+                setEditCollector({
+                    name: res.data.name
+                });
+
+                records = res.data.records;
+
+                comments = res.data.comments
+
+                loading = false;
+            } catch (err) {
+                console.error(err.message ? err.message : err.response);
+            }
+        }
+    }, [auth]);
+
+    const handleCollectorChange = (e) => {
+        setEditCollector({
+            ...editCollector,
+            [e.target.id]: e.target.value
+        });
+    };
+
+    const handleUserChange = (e) => {
+        setEditUser({
+            ...editUser,
+            [e.target.id]: e.target.value
+        });
+    };
+
+    
+
+
+
     return(
         <h1>EditCollector</h1>
     )
