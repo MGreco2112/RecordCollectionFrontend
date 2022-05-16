2edb4627c4f2c7cd3a2c66d703187c412378f953
Matthew Greco <mgreco2112@tutanota.com>
2022-05-16 19:39:09 -0400

    working on editing collector/user

--- src/components/Collector/EditCollector.js
+++ src/components/Collector/EditCollector.js
@@ -6,51 +6,79 @@ import { AuthContext } from "../Providers/AuthProvider";
 import axios from "axios";
 import Form from "../common/Form";
 import InlineInputContainer from "../common/InlineInputContainer";
+import Input from "../common/Input";
 import Button from "../common/Button";
 import Container from "../common/Container";
 
 const EditCollector = (props) => {
 
+    const navigate = useNavigate();
+
     const [auth] = useContext(AuthContext);
 
     const [editCollector, setEditCollector] = useState({
-        name: ""
+        name: "",
+        records: [],
+        comments: []
     });
+
+    const [newPassword, setNewPassword] = useState("");
 
     const [editUser, setEditUser] = useState({
         username: "",
         password: ""
     });
 
-    let records = [];
+    const [editRecords, setEditRecords] = useState();
 
-    let comments = [];
+    const [editComments, setEditComments] = useState();
 
     let loading = true;
 
-    useEffect( async () => {
-        if (auth.token) {
+    useEffect(() => {
 
-            try {
-                const res = await axios.get(`${apiHostURL}/api/collectors/currentCollector`, {
-                    headers: {
-                        Authorization: `Bearer ${auth.token}`
-                    }
-                });
+        const _apiCalls = async () => {
 
-                setEditCollector({
-                    name: res.data.name
-                });
-
-                records = res.data.records;
-
-                comments = res.data.comments
-
-                loading = false;
-            } catch (err) {
-                console.error(err.message ? err.message : err.response);
+            if (auth.token) {
+    
+                try {
+                    const res = await axios.get(`${apiHostURL}/api/collectors/currentCollector`, {
+                        headers: {
+                            Authorization: `Bearer ${auth.token}`
+                        }
+                    });
+    
+                    const getUser = await axios.get(`${apiHostURL}/api/collectors/user/${auth.profile.id}`, {
+                        headers: {
+                            Authorization: `Bearer ${auth.token}`
+                        }
+                    });
+    
+                    setEditCollector({
+                        ...editCollector,
+                        name: res.data.name
+                    });
+    
+                    setEditUser({
+                        ...editUser,
+                        username: getUser.data.username,
+                        password: getUser.data.password
+                    });
+    
+                    setEditRecords(res.data.records);
+    
+                    setEditComments(res.data.comments);
+    
+                    loading = false;
+                } catch (err) {
+                    console.error(err.message ? err.message : err.response);
+                }
             }
         }
+
+        loading = true;
+
+        _apiCalls();
     }, [auth]);
 
     const handleCollectorChange = (e) => {
@@ -67,12 +95,88 @@ const EditCollector = (props) => {
         });
     };
 
-    
+    const handlePasswordChange = (e) => {
+        setNewPassword(e.target.value);
 
+        setEditUser({
+            ...editUser,
+            password: e.target.value
+        });
+    }
+
+    const onSubmit = () => {
+
+        _putUpdates(editCollector, editUser);
+
+    }
+
+    const _putUpdates = async (editCollector, editUser) => {
+        console.log(editCollector);
+
+        try {
+            const putCollector = await axios.put(`${apiHostURL}/api/collectors`, editCollector, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+
+            const putUser = await axios.put(`${apiHostURL}/api/collectors/user`, editUser, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+
+            navigate(`/collector/${auth.profile.username}`);
+        } catch (err) {
+            console.error(err.message ? err.message : err.response);
+        }
+    }
+    
+    console.log(editCollector);
 
 
     return(
-        <h1>EditCollector</h1>
+        <Container>
+            <h1>Edit Your Profile</h1>
+
+            {loading ?
+                <Form onSubmit={onSubmit} style={{marginTop: '1em'}}>
+                    <InlineInputContainer>
+                        <Input
+                            name="username"
+                            id="username"
+                            value={editUser.username}
+                            onChange={handleUserChange}
+                            label={"Username"}
+                            required
+                        />
+                    </InlineInputContainer>
+                    <InlineInputContainer>
+                        <Input
+                            name="password"
+                            id="password"
+                            value={newPassword}
+                            onChange={handlePasswordChange}
+                            label={"New Password"}
+                            placeholder="Leave blank for old password"
+                        />
+                    </InlineInputContainer>
+                    <InlineInputContainer>
+                        <Input
+                            name="name"
+                            id="name"
+                            value={editCollector.name}
+                            onChange={handleCollectorChange}
+                            label={"Display Name"}
+                            required
+                        />
+                    </InlineInputContainer>
+                    <Button>Submit</Button>
+                </Form>
+                :
+                <h1>loading...</h1>
+            }
+        </Container>
     )
 }
 
