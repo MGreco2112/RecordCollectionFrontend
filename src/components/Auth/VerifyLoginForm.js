f5b8959b3e76ff346b849fa3d1ecc97ffd3b9783
Matthew Greco <mgreco2112@tutanota.com>
2022-08-15 17:56:16 -0400

    created comments to outline logic flow of VerifyLoginForm component

--- src/components/Auth/VerifyLoginForm.js
+++ src/components/Auth/VerifyLoginForm.js
@@ -2,6 +2,18 @@ import React from "react";
 import VerifyLogin from "./VerifyLogin";
 
 const VerifyLoginForm = (props) => {
+
+    /**
+     * Have user input old login credentials
+     * send old login to backend with new user object fields
+     * if old login is valid
+     *      update user with new details
+     *      login user with new credentials via backend
+     *      return new user details and JWT to React
+     *      navigate Home
+     * else
+     *      Alert that credentaials have not been updated
+     */
     return(
         <h1>Verify Login</h1>
     );
